84e1ae5d19ef65e38be4d9e434d5889f99b47681
Juan Pablo Varela <68089938+Juan-Varela11@users.noreply.github.com>
2024-03-28 23:21:03 -0400

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import sklearn as sk
 from math import *
+import plotly
+import matplotlib.pyplot as plt
 
 # Load in data
 cdc_data = pd.read_csv('Death_rates_for_suicide__by_sex__race__Hispanic_origin__and_age__United_States_20240327.csv')
@@ -10,4 +12,6 @@ cdc_data = pd.read_csv('Death_rates_for_suicide__by_sex__race__Hispanic_origin__
 print(cdc_data.describe())
 print(cdc_data.shape)
 print(cdc_data.head(5))
+print(cdc_data.info(verbose=True))
 
+# Isolate data for Male Hispanic or Latino AND Female Hispanic or Latino instances
