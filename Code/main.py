7d59aaa3aa9d322e9c751599e0962c8056876ed3
Juan Pablo Varela <68089938+Juan-Varela11@users.noreply.github.com>
2024-04-04 22:52:05 -0400

    Update main.py
    Added section to create death_vs_year_fitextended.jpg

--- Code/main.py
+++ Code/main.py
@@ -10,11 +10,11 @@ from scipy import optimize
 cdc_data = pd.read_csv('Death_rates_for_suicide__by_sex__race__Hispanic_origin__and_age__United_States_20240327.csv')
 
 # Explore & describe complete cdc dataset
-print(cdc_data.describe())
-print(cdc_data.shape)
-print(cdc_data.head(1))
-print(cdc_data.columns.values)
-print(cdc_data.info(verbose=True))
+#print(cdc_data.describe())
+#print(cdc_data.shape)
+#print(cdc_data.head(1))
+#print(cdc_data.columns.values)
+#print(cdc_data.info(verbose=True))
 
 # Isolate crude data (unadjusted rates) for Male Hispanic or Latino AND Female Hispanic or Latino instances (regardless of race)
 male_hispanic_cdc = cdc_data[(cdc_data['STUB_LABEL'] == 'Male: Hispanic or Latino: All races') & (cdc_data['UNIT'] == 'Deaths per 100,000 resident population, crude')]
@@ -61,4 +61,32 @@ plt.xlabel('Year')
 plt.ylabel('# of Suicide Deaths')
 plt.legend(loc='best')
 plt.savefig(fname='deaths_vs_year_withfit.jpg')
+plt.close()
+
+# Extending the poly by 10 years, to see how the fit evolves
+time_future = pd.Series([2018,2019,2020,2021,2022,2023,2024,2025,2026,2027,2028])
+
+plt.scatter(male_hispanic_cdc['YEAR'],male_hispanic_cdc['ESTIMATE'],label='Male',c='blue')
+plt.scatter(female_hispanic_cdc['YEAR'],female_hispanic_cdc['ESTIMATE'],label='Female',c='red')
+plt.plot(male_hispanic_cdc['YEAR'], male_cdc_fit(male_hispanic_cdc['YEAR'],params_male[0],
+                                                 params_male[1],params_male[2],
+                                                 params_male[3],params_male[4]),
+                                                 label='Male data fit',c='blue')
+plt.plot(female_hispanic_cdc['YEAR'], female_cdc_fit(female_hispanic_cdc['YEAR'],params_female[0],
+                                                 params_female[1],params_female[2],
+                                                 params_female[3],params_female[4]),
+                                                 label='Female data fit',c='red')
+plt.plot(time_future, male_cdc_fit(time_future,params_male[0],
+                                                 params_male[1],params_male[2],
+                                                 params_male[3],params_male[4]),
+                                                 c='blue',linestyle = 'dashed')
+plt.plot(time_future, female_cdc_fit(time_future,params_female[0],
+                                                 params_female[1],params_female[2],
+                                                 params_female[3],params_female[4]),
+                                                 c='red',linestyle = 'dashed')
+plt.title('Predictive Trend with Current Fit')
+plt.xlabel('Year')
+plt.ylabel('# of Suicide Deaths')
+plt.legend(loc='best')
+plt.savefig(fname='deaths_vs_year_fitextended.jpg')
 plt.close()
